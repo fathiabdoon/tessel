2407c8ac036ba885ade983e6c095bed59a4a4241
Gabriel Lebec <glebec@gmail.com>
2014-09-22 18:55:56 -0400

    slight refactoring of visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -15,9 +15,12 @@ var triggerDelay = 10;
 var threshold = 0.01;
 var color = {
   off: [0x00, 0x00, 0x00],
+  green: [0xFF, 0x00, 0x00],
+  red: [0x00, 0xFF, 0x00],
   blue: [0x00, 0x00, 0xFF],
-  green: [0x00, 0xFF, 0x00],
-  red: [0xFF, 0x00, 0x00]
+  purple: [0x00, 0xFF, 0xFF],
+  yellow: [0xFF, 0xFF, 0x00],
+  cyan: [0xFF, 0x00, 0xFF]
 };
 var currentColor = color.off;
 
@@ -26,40 +29,31 @@ neopixels.animate( leds, Buffer.concat(setAll(leds, color.off)) );
 
 // when ready
 ambient.on('ready', function () {
+  // ready animation for fun
+  neopixels.animate( leds, Buffer.concat(tracer(leds)) );
   // Set a sound level trigger float between 0 and 1
   ambient.setSoundTrigger(threshold);
 
   ambient.on('sound-trigger', function(data) {
-    console.log('Something happened with sound: ', data);
+    console.log('Tripped sound threshold at level ', data);
+
     var newColor = convertColor(data);
     neopixels.animate( leds, Buffer.concat(setAll(leds, newColor)) );
+
+    // Clear it
+    ambient.clearSoundTrigger();
+
+    //After delay reset sound trigger
+    setTimeout(function () {
+      ambient.setSoundTrigger(threshold);
+    }, triggerDelay);
   });
-
-  // Clear it
-  ambient.clearSoundTrigger();
-
-  //After delay reset sound trigger
-  setTimeout(function () {
-    ambient.setSoundTrigger(threshold);
-  }, triggerDelay);
 });
 
 // error logging
 ambient.on('error', function (err) {
   console.log(err);
 });
-
-// set leds to color
-function setAll (numLEDs, color) {
-  var buf = new Buffer(numLEDs * 3);
-  for (var i = 0; i < buf.length; i += 3) {
-    buf[i] = color[0];
-    buf[i+1] = color[1];
-    buf[i+2] = color[2];
-  }
-  currentColor = color;
-  return [buf];
-}
 
 function convertColor (decimal) {
   var max = 0.1;
@@ -76,4 +70,42 @@ function convertColor (decimal) {
     output[2] = 0xFF - output[1]; // B
   }
   return output;
+}
+
+// set leds to color
+function setAll (numLEDs, color) {
+  var buf = new Buffer(numLEDs * 3);
+  for (var i = 0; i < buf.length; i += 3) {
+    buf[i] = color[0];
+    buf[i+1] = color[1];
+    buf[i+2] = color[2];
+  }
+  currentColor = color;
+  return [buf];
+}
+
+// An example animation
+function tracer(numLEDs) {
+  var trail = 20;
+  var arr = new Array(numLEDs);
+  for (var i = 0; i < numLEDs; i++) {
+    var buf = new Buffer(numLEDs * 3);
+    buf.fill(0);
+    for (var col = 0; col < 3; col++){
+      for (var k = 0; k < trail; k++) {
+        buf[(3*(i+numLEDs*col/3)+col+1 +3*k)] = 0xFF*(trail-k)/trail;
+      }
+    }
+    arr[i] = buf;
+  }
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    newArr.push(arr[i]);
+    newArr.push(arr[i]);
+    newArr.push(arr[i]);
+    newArr.push(arr[i]);
+    newArr.push(arr[i]);
+    newArr.push(arr[i]);
+  }
+  return newArr;
 }
